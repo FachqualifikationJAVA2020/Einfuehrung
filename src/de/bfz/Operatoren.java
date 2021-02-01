9bfd604edbe60ecbfebc16bf79ecbf3ebff80e9c
Christian Richter <Christian.Richter1987@gmail.com>
2021-02-01 10:49:50 +0100

    Add Variablen.java

---
+++ src/de/bfz/Operatoren.java
@@ -0,0 +1,12 @@
+package de.bfz;
+
+public class Operatoren {
+    public static void main(String[] args) {
+        /*
+         *  Es gibt die Rechenoperatoren
+         *  + - * /
+         *  diese gelten für alle Zahlentypen
+         *
+         */
+    }
+}
